728b36e9ba1ad00201ed640d63afa37816c18888
Quypham <145001011+ImNotTurtle@users.noreply.github.com>
2024-03-21 10:07:42 +0700

    Add files via upload

---
+++ Source_Code/STM32_Source_Code/inc/Utils.h
@@ -0,0 +1,16 @@
+/*
+ * Utils.h
+ *
+ *  Created on: Mar 16, 2024
+ *      Author: Administrator
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+#include <stdint.h>
+
+
+uint32_t Timer_CalcTimeDiff(uint32_t time1, uint32_t time2);
+
+
+#endif /* UTILS_H_ */
